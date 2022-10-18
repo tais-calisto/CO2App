d55f9b1daef7ab246f923c6f1c3ef786408453fa
Tais Calisto <taiscalisto97@gmail.com>
2022-10-17 20:31:59 -0400

    Deleted node:test

--- src/pages/calculate/index.spec.tsx
+++ src/pages/calculate/index.spec.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
 import Calculate from '.'
-import { it } from 'node:test'
 
 it('See result text', () => {
   render(
