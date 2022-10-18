6b02d61fccb7f21e47ee77683a3597aad11d5193
Tais Calisto <taiscalisto97@gmail.com>
2022-10-17 21:06:24 -0400

    Added result page's styles

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,15 +1,18 @@
 import { NextPage } from 'next'
 import { useGlobalContext } from '@/context/AppContext'
 import Header from '@/components/header/Header'
+import { ResultStyle } from '@/styles/resultStyle'
 
 const Result: NextPage = () => {
   const { result } = useGlobalContext()
   return (
-    <>
+    <ResultStyle>
       <Header />
-      <h2>Resultado:</h2>
-      <h3>A estimativa de emissão de carbono é de {result} kg</h3>
-    </>
+      <section>
+        <h1>Resultado:</h1>
+        <h3>A estimativa de emissão de carbono é de {result} kg</h3>
+      </section>
+    </ResultStyle>
   )
 }
 
---
+++ src/styles/resultStyle.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ResultStyle = styled.main`
+  section {
+    padding-left: 5rem;
+    padding-top: 5rem;
+  }
+  h1 {
+    padding-bottom: 1rem;
+  }
+`
